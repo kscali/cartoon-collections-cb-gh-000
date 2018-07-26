4665fc3bed26fb52d22b0646a913c5978136ef3c
Koy Saeteurn <kosatun@aol.com>
2018-07-26 18:49:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,8 @@
-
+def roll_call_dwarves(array)
+  array.each_with_index do |dwarf, index| 
+    puts "#{index + 1} #{dwarf}"
+   end  
+end
 
 def summon_captain_planet# code an argument here
   # Your code here
