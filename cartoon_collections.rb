90175cc62a220f9c620bf6bf37e72e101d63e9b8
Koy Saeteurn <kosatun@aol.com>
2018-07-26 19:21:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,16 +13,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese = ""
-  while i < array.length 
-    if array[i] == cheese_types[i]
-      cheese << array[i]
-   else 
-      nil
-   end 
-    i += 1
-  cheese   
-  end
+  array.include? {|cheese| cheese_types}
 end
